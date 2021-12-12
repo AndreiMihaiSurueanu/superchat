4b009b7063a9c8461637d802e56221842067c108
andrei <git1@email.com>
2021-12-12 13:17:55 +0100

    Added imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import './App.css';
+import firebase from 'firebase';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  // your config
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
