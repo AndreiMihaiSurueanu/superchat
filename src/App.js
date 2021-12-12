4ed668643e4941b8788c192b73f020c48650ac31
andrei <git1@email.com>
2021-12-12 13:30:20 +0100

    Added Firebase project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config
+  apiKey: "AIzaSyDxxfzNzUWCofWqjv2Qg2Vbwi7-jsoLzAA",
+  authDomain: "react-projects-9.firebaseapp.com",
+  projectId: "react-projects-9",
+  storageBucket: "react-projects-9.appspot.com",
+  messagingSenderId: "891378795148",
+  appId: "1:891378795148:web:9565f42c72483b1f27f1a9"
 })
 
 const auth = firebase.auth();
